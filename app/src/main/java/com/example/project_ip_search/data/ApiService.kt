d13cac0ebba2fe6c75acd2001e4f3497fc327d0b
antvill77 <antvillot77@hotmail.com>
2026-02-24 03:45:10 -0600

    Create models, constants an interface

--- app/src/main/java/com/example/project_ip_search/data/ApiService.kt
+++ app/src/main/java/com/example/project_ip_search/data/ApiService.kt
@@ -1,91 +1,13 @@
 package com.example.project_ip_search.data
 
-data class ApiService(
-    val area: Area,
-    val asn: Asn,
-    val city: City,
-    val continent: Continent,
-    val country: Country,
-    val currency: Currency,
-    val ip: String,
-    val location: Location,
-    val postcode: Any,
-    val security: Security,
-    val status: String,
-    val time: Time,
-    val type: String
-)
+import com.example.project_ip_search.model.IdModel
+import com.example.project_ip_search.util.Constants.Companion.API_KEY
+import com.example.project_ip_search.util.Constants.Companion.ENDPOINT
+import retrofit2.Response
+import retrofit2.http.GET
 
-data class Area(
-    val code: String,
-    val geonameid: Int,
-    val name: String
-)
+interface ApiService{
 
-data class Asn(
-    val number: Int,
-    val organisation: String
-)
-
-data class City(
-    val geonameid: Int,
-    val name: String,
-    val population: Int
-)
-
-data class Continent(
-    val code: String,
-    val geonameid: Int,
-    val name: String
-)
-
-data class Country(
-    val area_size: String,
-    val capital: String,
-    val code: String,
-    val flag: Flag,
-    val geonameid: Int,
-    val is_in_eu: Boolean,
-    val languages: Languages,
-    val name: String,
-    val phone_code: String,
-    val population: Int,
-    val tld: String
-)
-
-data class Currency(
-    val code: String,
-    val name: String
-)
-
-data class Flag(
-    val emoji: String,
-    val `file`: String,
-    val unicode: String
-)
-
-data class Languages(
-    val el: String,
-    val en: String,
-    val tr: String
-)
-
-data class Location(
-    val latitude: Double,
-    val longitude: Double
-)
-
-data class Security(
-    val is_crawler: Boolean,
-    val is_proxy: Boolean,
-    val is_threat: Boolean,
-    val is_tor: Boolean
-)
-
-data class Time(
-    val code: String,
-    val gmt_offset: Int,
-    val is_daylight_saving: Boolean,
-    val timezone: String
-)
-
+    @GET(ENDPOINT + API_KEY)
+    suspend fun getId(): Response<IdModel>
+}
--- app/src/main/java/com/example/project_ip_search/util/Constants.kt
+++ app/src/main/java/com/example/project_ip_search/util/Constants.kt
@@ -5,6 +5,6 @@ class Constants {
     companion object{
         const val BASE_URL = "https://ip-geo-location.p.rapidapi.com/"
         const val ENDPOINT = "ip/37.140.128.10"
-        const val API_KEY = "2de9fa8d7amsh42790fa255456e6p18e28bjsn82a0f3ff2781"
+        const val API_KEY = "?rapidapi-key=2de9fa8d7amsh42790fa255456e6p18e28bjsn82a0f3ff2781"
     }
 }
